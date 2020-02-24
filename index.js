0664e09bcb612a78265d99bebe661e64cfa9706a
tdotmdot00 <51458672+tdotmdot00@users.noreply.github.com>
2020-02-24 10:11:50 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const SlackBot = require('slackbots');
 const axios = require('axios');
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 const bot = new SlackBot({
     token: `${process.env.BOT_TOKEN}`,
